a00f253f6decbd48833c85412902180a2578b465
ChrisIrvine <christopher@irvinegroup.eu>
2018-11-17 16:52:54 +0000

    Toolbar created. Able to draw rectangles.

--- src/Delegate/Main.java
+++ src/Delegate/Main.java
@@ -1,18 +1,100 @@
 package Delegate;
 
+import Model.MandelbrotCalculator;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.CanvasBuilder;
-import javafx.scene.layout.Pane;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.awt.Point;
+import java.util.zip.ZipEntry;
+
 
 public class Main extends Application {
 
+    private BorderPane mandelPane = new BorderPane();
+    private int startX;
+    private int endX;
+    private int startY;
+    private int endY;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Pane mandelPane = new Pane();
+        //Add items to pane
+        mandelPane.setTop(createToolbar());
+        mandelPane.setCenter(paintMandel());
+
+        primaryStage.setTitle("Mandelbrot Set");
+        Scene scene = new Scene(
+                mandelPane, Configuration.WIDTH + (2 * Configuration.XOFFSET)
+                , Configuration.HEIGHT + (2 * Configuration.YOFFSET) + 50
+        ); //the additional 50 pixels added to the Height is to accommodate a
+        scene.setFill(Color.WHITE);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private VBox createToolbar() {
+        ToolBar toolBar = new ToolBar();
+
+        Button changeColor = new Button("Change Color");
+        Button undo = new Button("Undo");
+        Button redo = new Button("Redo");
+        Button reset = new Button("Reset");
+        Button updateIterations = new Button("Update Iterations");
+
+        TextField iterations = new TextField();
+        iterations.setText(String.valueOf(
+                MandelbrotCalculator.getInitialMaxIterations()
+        ));
+
+        final ToggleGroup zp = new ToggleGroup();
+        RadioButton zoom = new RadioButton("Zoom");
+        RadioButton pan = new RadioButton("Pan");
+        zoom.setToggleGroup(zp);
+        pan.setToggleGroup(zp);
+
+        toolBar.getItems().add(zoom);
+        toolBar.getItems().add(pan);
+        toolBar.getItems().add(new Separator());
+        toolBar.getItems().add(undo);
+        toolBar.getItems().add(reset);
+        toolBar.getItems().add(changeColor);
+        toolBar.getItems().add(new Separator());
+        toolBar.getItems().add(redo);
+        toolBar.getItems().add(new Separator());
+        toolBar.getItems().add(iterations);
+        toolBar.getItems().add(updateIterations);
+
+        toolBar.prefWidthProperty().bind(mandelPane.widthProperty());
+
+        return new VBox(toolBar);
+    }
+
+    private Canvas paintMandel() {
+        final Rectangle zoom = new Rectangle(0, 0, 0, 0);
+        final Point anchor = new Point();
+        //final Line pan = new Line();
+
+        //Create Mandelbrot Set Image
         Canvas canvas = CanvasBuilder
                 .create()
                 .height(Configuration.HEIGHT)
@@ -26,21 +108,30 @@ public class Main extends Application {
                 Controller.generateMandel()
         );
 
-        mandelPane.getChildren().add(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        primaryStage.setTitle("Mandelbrot Set");
-        Scene scene = new Scene(
-                mandelPane, Configuration.WIDTH + (2 * Configuration.XOFFSET)
-                , Configuration.HEIGHT + (2 * Configuration.YOFFSET) + 50
-        ); //the additional 50 pixels added to the Height is to accommodate a
-        //navigation bar
-        scene.setFill(Color.WHITE);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+        canvas.setOnMousePressed(event -> {
+            anchor.setLocation(event.getX(), event.getY());
+            zoom.setX(event.getX());
+            zoom.setY(event.getY());
+            zoom.setFill(null);
+            zoom.setStroke(Color.ORANGE);
+            zoom.getStrokeDashArray().add(10.0);
+            mandelPane.getChildren().add(zoom);
+        });
 
+        canvas.setOnMouseDragged(event -> {
+            zoom.setWidth(Math.abs(event.getX() - anchor.getX()));
+            zoom.setHeight(Math.abs(event.getY() - anchor.getY()));
+            zoom.setX(Math.min(anchor.getX(), event.getX()));
+            zoom.setY(Math.min(anchor.getY(), event.getY()));
+        });
 
-    public static void main(String[] args) {
-        launch(args);
+        canvas.setOnMouseReleased(event -> {
+            Controller.paintMandelbrot(gc, Controller.generateMandel());
+            mandelPane.setCenter(canvas);
+        });
+
+        return canvas;
     }
 }
